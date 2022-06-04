5ed6df9f8b9e6c365c89c131c0c3e879646f9533
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2022-06-04 15:54:26 +0530

    fixed a syntax error

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -4,7 +4,7 @@ const MealItem = props => {
   const price = `$${props.price.toFixed(2)}`;
 
   return (
-    <li className="styles.meal">
+    <li className={styles.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={styles.description}>{props.description}</div>
