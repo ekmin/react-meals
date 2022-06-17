eddc73def7e51990198bfc2f83ad9a6e55bc1990
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2022-06-17 08:41:14 +0530

    added a error state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,12 +7,18 @@ import styles from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-movies-c391e-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong")
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -30,13 +36,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+      fetchMeals().catch(error => {
+        setIsLoading(false);
+        setHttpError(error.message)
+      }); 
   }, []);
 
   if (isLoading) {
     return (
       <section className={styles.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.MealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
